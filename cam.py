a64d42410190a33e0186d0ae1774caef28dfa3cd
Alexandre Kandalintsev <spam@messir.net>
2014-10-11 16:17:44 +0000

    cam.py: clean-ups

--- cam.py
+++ cam.py
@@ -5,22 +5,16 @@ from time import sleep
 from sys import exit
 import atexit
 
-XCAM = 24
-MIN_DOWN = 2500
-MAX_UP = 690
-VERT_CENTER = 1600
+MIN, CENTER, MAX = 0,1,2
 
-YCAM = 23
-
-class Cam:
-  def __init__(self, pin, servo_min, servo_max, servo_center):
+class MyServo:
+  def __init__(self, pin, servo_range, angles):
     servo = PWM.Servo()
 
     self.servo = servo
     self.pin = pin
-    self.servo_min = servo_min
-    self.servo_max = servo_max
-    self.servo_center = servo_center
+    self.servo_range = servo_range
+    self.angles = angles
     atexit.register(self.reset)
 
     def set_pos(v, t):
@@ -30,21 +24,13 @@ class Cam:
     self.set_pos = set_pos
 
   def reset(self):
-    self.set_pos(self.servo_center, 0.4)
+    self.set_pos(self.servo_range[CENTER], 0.4)
 
-  def set_min(self):
-    self.servo.set_servo(self.pin, self.servo_min)
-    sleep(2)
-    self.servo.stop_servo(self.pin)
+  def set_angle(angle):
+    
 
-  def set_max(self):
-    self.servo.set_servo(self.pin, self.servo_max)
-    sleep(2)
-    self.servo.stop_servo(self.pin)
-
-
-cam = Cam(pin=24, servo_min=2500, servo_max=700, servo_center=1400)
-cam = Cam(pin=23, servo_min=2500, servo_max=700, servo_center=1600)
+xcam = MyServo(pin=24, servo_range=[2500, 1400, 700], angles=[-90, 0, +90])
+ycam = MyServo(pin=23, servo_range=[2500, 1600, 700], angles=[-90, 0, +90])
 #cam.reset()
 #cam.set_min()
 #cam.set_max()
