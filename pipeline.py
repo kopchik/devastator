ee792235c5e52d11b6048f24c02c2eb1c9d74cea
Alexandre Kandalintsev <spam@messir.net>
2014-11-01 23:57:03 +0000

    preliminary version of pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 import atexit
-from subprocess import Popen, DEVNULL
+import csv
+from subprocess import Popen, check_call, DEVNULL
+from os import path
 
-OUTPUT = "/home/stream"
+OUTPUT = "/home/stream/"
+SEGMENTS_INDEX = OUTPUT + "segments.csv"
 PIPELINE = """
 raspivid -w 800 -h 600 -o - -t 9999999 \
  | ffmpeg -y -i - -c:v copy -map 0:0 \
-     -f ssegment -segment_time 10 -segment_format mpegts \
-     -segment_list out.csv -segment_list_flags live -segment_list_size 10 \
-     {}/%08d.ts
+     -f ssegment -segment_time 10 \
+     -segment_list {idx} -segment_list_flags live -segment_list_size 10 \
+     -loglevel warning \
+     {out}/%08d.mp4
 """
-cmd = PIPELINE.format(OUTPUT)
-pipeline = Popen(cmd, shell=True)
-atexit(pipeline.terminate)
-pipeline.wait()
+#-segment_format_options movflags=+faststart \
+CONVERT = """ffmpeg -y -i {inpt} -vcodec copy -acodec copy {out}"""
+
+def start():
+  cmd = PIPELINE.format(out=OUTPUT, idx=SEGMENTS_INDEX)
+  pipeline = Popen(cmd, shell=True)
+  def cleanup():
+    check_call("rm -rf %s/*" % OUTPUT, shell=True)
+  atexit(register(cleanup)
+  atexit.register(pipeline.terminate)
+  return pipeline
+
+
+def get_latest():
+  with open(SEGMENTS_INDEX, 'rt') as fd:
+    for f, start, end in csv.reader(fd):
+      pass
+  print("the latest segment is", f)
+  return f
