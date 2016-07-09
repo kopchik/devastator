16ee55bb3c2e52f4fac527208e17eaac464ad180
Alexandre Kandalintsev <exe.sre@gmail.com>
2016-07-09 21:17:17 +0000

    switch from RPIO to RPi

--- myservo.py
+++ myservo.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-
-from RPIO import PWM
-PWM.set_loglevel(PWM.LOG_LEVEL_ERRORS)
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 
 from operator import itemgetter
 from bisect import bisect_left
 from useful.timer import Timer
 import atexit
+import time
 
 
 class Interpolate:
@@ -24,36 +24,63 @@ class Interpolate:
     return self.Y[i] + self.slopes[i] * (x - self.X[i])
 
 
+
 class MyServo:
-  def __init__(self, pin, map):
+  def __init__(self, name, pin, map, reset=True):
     X, Y = [], []
     for x, y in sorted(map.items(), key=itemgetter(0)):
       X.append(x)
       Y.append(y)
     self.map = Interpolate(X, Y)
 
-    self.servo = PWM.Servo()
+    self.name = name
     self.pin = pin
+    GPIO.setup(pin, GPIO.OUT)
+    self.servo = GPIO.PWM(pin, 50)
+    self.value = 0
+
+
     atexit.register(self.reset)
     atexit.register(self.stop)
-    self.timer = Timer(self.stop, 5)
-    self.timer.start()
+    atexit.register(GPIO.cleanup)
+    if reset:
+      self.reset()
+    #self.timer = Timer(self.stop, 1)
+    #self.timer.start()
 
   def _set(self, v):
-    v = int(v) // 10 * 10  # TODO: round to 10us
-    self.servo.set_servo(self.pin, v)
-    self.timer.restart()
+    self.servo.start(v)  #self.servo.ChangeDutyCycle(v)
+    #self.timer.restart()
     # TODO: limit command rate because charger
     # cannot give enough power for two servos
-    #time.sleep(0.3)
+    #time.sleep(0.1)
 
   def stop(self):
-    self.servo.stop_servo(self.pin)
+    self.servo.stop()
 
   def reset(self):
     self._set(self.map[0])
 
   def set(self, value):
-    pos = self.map[value]
-    self._set(pos)
+    try:
+      raw = self.map[value]
+      self._set(raw)
+      self.value = value
+      print("{name} {val} {raw}\r".format(name=self.name, val=value, raw=raw))
+    except IndexError:
+      pass
 
+  def inc(self, val):
+    return self.set(self.value + val)
+
+  def dec(self, val):
+    return self.set(self.value - val)
+
+  def reduce(self, value):
+    if abs(self.value - value) <= value:
+      self.set(0)
+    else:
+      if self.value > 0:
+        self.dec(value)
+      else:
+        self.inc(value)
